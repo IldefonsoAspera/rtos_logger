05842f729766b46a9007530acd3c38fc6a7c33be
Ildefonso Aspera <ildefonso.aspera@gmail.com>
2021-11-30 00:14:19 +0100

    Optimized logger functions, needs testing

--- Src/log.c
+++ Src/log.c
@@ -104,41 +104,6 @@ static void log_fifo_reset(log_fifo_t *pFifo)
 }
 
 
-static inline uint8_t process_number_decimal(uint32_t number, char *output)
-{
-    uint32_t divider = 1000000000UL;
-    uint8_t i;
-
-    for(i=0; i<10; i++)
-    {
-        output[i] = 0x30 + number/divider;
-        number %= divider;
-        divider /= 10;
-    }
-
-    i = 0;
-    while(i < 10 && output[i] == 0x30)
-        i++;
-
-    if(i == 10)     // Case when all digits are 0 should print one digit ('0')
-        return 1;
-    else            // Return number of digits to print without leading zeroes
-        return 10-i;
-}
-
-
-static inline void process_number_hex(uint32_t number, char *output, uint8_t n_digits)
-{
-    char hexVals[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-    int8_t i;
-
-    for(i = sizeof(number)*2 - 1; i > -1; i--)
-    {
-        output[i] = hexVals[number & 0x0F];
-        number >>= 4;
-    }
-}
-
 
 #if LOG_SUPPORT_ANSI_COLOR
 static void set_color(enum log_color color)
@@ -164,47 +129,59 @@ static void set_color(enum log_color color)
 #endif
 
 
-static void proc_string(char *string, uint32_t length)
+static void process_string(char *string, uint32_t length)
 {
     vcp_send(string, length);
 }
 
 
-static void proc_uint_dec(uint32_t number)
+static void process_hexadecimal(uint32_t number, uint8_t n_digits)
 {
-    char output[10];
-    uint8_t n_digits;
+    const char hexVals[16] = {'0','1','2','3','4','5','6','7',
+                              '8','9','A','B','C','D','E','F'};
+    char output[n_digits];
+    int8_t i;
 
-    n_digits = process_number_decimal(number, output);
-    proc_string(&output[10 - n_digits], n_digits);
+    // Fill char array starting at the end
+    for(i = n_digits-1 ; i > -1; i--)
+    {
+        output[i] = hexVals[number & 0x0F];
+        number >>= 4;
+    }
+
+    proc_string(output, n_digits);
 }
 
 
-static void proc_hex(uint32_t number, uint8_t n_digits)
+static void process_decimal(uint32_t number, bool isNegative)
 {
-    char output[sizeof(number)*2];
+    char* output[11];
+    uint32_t divider = 1000000000UL;
+    uint8_t i = 0;
 
-    process_number_hex(number, output, n_digits);
-    proc_string(&output[8 - n_digits], n_digits);
+    if(isNegative)
+        output[i++] = '-';
+
+    if(number)
+    {
+        while(number < divider)
+            divider /= 10;
+
+        while(number > 0)
+        {
+            output[i++] = 0x30 + number/divider;
+            number %= divider;
+            divider /= 10;
+        }
+    }
+    else
+        output[i++] = '0';
+
+    proc_string(output, i);
 }
 
 
-static void proc_sint_dec(int32_t number)
-{
-    char output[11];
-    bool is_negative = number < 0;
-    uint8_t n_digits;
-
-    if(is_negative)
-        number = -number;
-
-    n_digits = process_number_decimal(number, &output[1]);
-
-    if(is_negative)
-        output[10 - n_digits++] = '-';
-
-    proc_string(&output[11 - n_digits], n_digits);
-}
+// TODO process array (stack/no stack)
 
 
 void _log_var(uint32_t number, enum log_data_type type, enum log_color color)
@@ -258,22 +235,25 @@ void log_flush(void)
             proc_string((char*)item.data, item.str_len);
             break;
         case LOG_UINT_DEC:
-            proc_uint_dec(item.data);
+            process_decimal(item.data, false);
             break;
         case LOG_INT_DEC:
-            proc_sint_dec((int32_t)item.data);
+            if((int32_t)item.data < 0)
+                process_decimal((uint32_t)(-((int32_t)item.data)), true);
+            else
+                process_decimal(item.data, false);
             break;
         case LOG_HEX_2:
-            proc_hex(item.data, 2);
+            process_hexadecimal(item.data, 2);
             break;
         case LOG_HEX_4:
-            proc_hex(item.data, 4);
+            process_hexadecimal(item.data, 4);
             break;
         case LOG_HEX_8:
-            proc_hex(item.data, 8);
+            process_hexadecimal(item.data, 8);
             break;
         case LOG_CHAR:
-            proc_string((char*)&item.data, 1);
+            process_string((char*)&item.data, 1);
         }
     }
 }
